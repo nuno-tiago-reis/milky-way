29b5673e649be344ae1254d564ac840215b31f41
Tiago Reis <g_tiago_1991@hotmail.com>
2014-11-23 22:02:05 +0000

    Added splitscreen HUD. Added a new track.

--- Milky Way/Assets/Scripts/SpaceShip/SpaceshipController.cs
+++ Milky Way/Assets/Scripts/SpaceShip/SpaceshipController.cs
@@ -6,6 +6,9 @@ using System.IO;
 
 public class SpaceshipController : MonoBehaviour {
 
+	/* Spaceships ID */ 
+	public int id;
+
 	/* Spaceships Health */ 
 	public float health
 	{ get; protected set; }
@@ -25,6 +28,16 @@ public class SpaceshipController : MonoBehaviour {
 	public float speed
 	{ get; protected set; }
 	public float steeringAngle
+	{ get; protected set; }
+
+	public Vector3 up
+	{ get; protected set; }
+	public Vector3 right
+	{ get; protected set; }
+	public Vector3 forward
+	{ get; protected set; }
+
+	public float directionInterpolator
 	{ get; protected set; }
 	
 	public Transform backLeftCorner
@@ -57,26 +70,6 @@ public class SpaceshipController : MonoBehaviour {
 		this.backRightCorner = this.transform.FindChild("Back Right Corner");
 		this.frontLeftCorner = this.transform.FindChild("Front Left Corner");
 		this.frontRightCorner = this.transform.FindChild("Front Right Corner");
-
-		if(this.name == "Spaceship 1") {
-
-			AddAbility("Shield");
-
-			GameObject ability = null;
-			
-			if(abilityInventory.TryGetValue("Shield", out ability)) {
-				
-				AbilityController abilityController = ability.GetComponent<RocketController>();
-				
-				if(abilityController != null) {
-					
-					abilityController.Activate(this.transform);
-					
-					abilityInventory.Remove(abilityController.abilityName);
-				}
-			}
-		}
-			
 	}
 	#endregion
 
@@ -115,82 +108,88 @@ public class SpaceshipController : MonoBehaviour {
 		RaycastHit frontRightHit;
 		
 		// Cast Rays from each of the Spaceships Corners heading towards the Track
-		bool backLeftRay = Physics.Raycast(backLeftCorner.position, Vector3.down, out backLeftHit, 5.0f, 1 << LayerMask.NameToLayer("Tracks"));
-		bool backRightRay = Physics.Raycast(backRightCorner.position, Vector3.down, out backRightHit, 5.0f, 1 << LayerMask.NameToLayer("Tracks"));
-		bool frontLeftRay = Physics.Raycast(frontLeftCorner.position, Vector3.down, out frontLeftHit, 5.0f, 1 << LayerMask.NameToLayer("Tracks"));
-		bool frontRightRay = Physics.Raycast(frontRightCorner.position, Vector3.down, out frontRightHit, 5.0f, 1 << LayerMask.NameToLayer("Tracks"));
+		bool backLeftRay = Physics.Raycast(backLeftCorner.position, -this.transform.up, out backLeftHit, 25.0f, 1 << LayerMask.NameToLayer("Tracks"));
+		bool backRightRay = Physics.Raycast(backRightCorner.position, -this.transform.up, out backRightHit, 25.0f, 1 << LayerMask.NameToLayer("Tracks"));
+		bool frontLeftRay = Physics.Raycast(frontLeftCorner.position, -this.transform.up, out frontLeftHit, 25.0f, 1 << LayerMask.NameToLayer("Tracks"));
+		bool frontRightRay = Physics.Raycast(frontRightCorner.position, -this.transform.up, out frontRightHit, 25.0f, 1 << LayerMask.NameToLayer("Tracks"));
 		
 		if(backLeftRay == true && backRightRay == true && frontLeftRay == true && frontRightRay == true) {
 			
 			// If there is a Collision, adjust the Spaceships Rotation
 			if(backLeftHit.collider.tag == "Track" && backRightHit.collider.tag == "Track" && frontLeftHit.collider.tag == "Track"  && frontRightHit.collider.tag == "Track" ) {
 				
-				Vector3 up = Vector3.Cross(backRightHit.point  - Vector3.up, backLeftHit.point   - Vector3.up) +
-					Vector3.Cross(backLeftHit.point   - Vector3.up, frontLeftHit.point  - Vector3.up) +
-						Vector3.Cross(frontLeftHit.point  - Vector3.up, frontRightHit.point - Vector3.up) +
-						Vector3.Cross(frontRightHit.point - Vector3.up, backRightHit.point  - Vector3.up);
-				up.Normalize();
+				this.up = 
+					Vector3.Cross(backRightHit.point - backRightHit.normal, backLeftHit.point - backLeftHit.normal) +
+					Vector3.Cross(backLeftHit.point - backLeftHit.normal, frontLeftHit.point - frontLeftHit.normal) +
+					Vector3.Cross(frontLeftHit.point - frontLeftHit.normal, frontRightHit.point - frontRightHit.normal) +
+					Vector3.Cross(frontRightHit.point - frontRightHit.normal, backRightHit.point  - backRightHit.normal);
+				this.up.Normalize();
 				
-				Vector3 right = this.transform.right;
-				right.Normalize();
+				this.right = this.transform.right;
+				this.right.Normalize();
 				
-				Vector3 forward = Vector3.Cross(right, up);
-				forward.Normalize();
-				
-				// Adjust the Spaceships Rotation so that it's parallel to the Track
-				this.transform.LookAt(this.transform.position + forward * 5.0f, up);
+				this.forward = Vector3.Cross(right, up);
+				this.forward.Normalize();
+
+				this.directionInterpolator = 0.0f;
 			}
 		}
+
+		this.directionInterpolator += Time.deltaTime * 2.0f;
+
+		Vector3 interpolatedUp = Vector3.Slerp(this.transform.up, this.up, directionInterpolator);
+		Vector3 interpolatedRight = Vector3.Slerp(this.transform.right, this.right, directionInterpolator);
+		Vector3 interpolatedForward = Vector3.Slerp(this.transform.forward, this.forward, directionInterpolator);
+
+		// Adjust the Spaceships Rotation so that it's parallel to the Track
+		this.transform.LookAt(this.transform.position + interpolatedForward * 5.0f, interpolatedUp);
 		
 		// Spaceships Position Adjustments
 		RaycastHit centerHit;
 		
 		// Cast a Ray from he Spaceships Center heading towards the Track
-		if(Physics.Raycast(this.transform.position, -this.transform.up, out centerHit, 3.0f, 1 << LayerMask.NameToLayer("Tracks")) == true) {
+		if(Physics.Raycast(this.transform.position, -this.transform.up, out centerHit, 5.0f, 1 << LayerMask.NameToLayer("Tracks")) == true) {
 			
 			// If there is a Collision, adjust the Spaceships Position
 			if(centerHit.collider.tag == "Track") {
 				
 				// Adjust the Spaceships Position so that it's slightly above the Track.
-				this.transform.position = centerHit.point + this.transform.up * 2.0f;
+				this.transform.position = centerHit.point + this.transform.up * 3.0f;
 			}
 		}
-
-		if(this.name == "Spaceship 0") {
 		
-			// User Input
-			float horizontalAxis = Input.GetAxis("Horizontal");
-			float verticalAxis = Input.GetAxis("Vertical");
+		// User Input
+		float horizontalAxis = Input.GetAxis("Horizontal");
+		float verticalAxis = Input.GetAxis("Vertical");
+		
+		// Update the Spaceships Acceleration
+		
+		// Accelerator
+		if((Input.GetKey(KeyCode.W) == true || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) == true) && Input.GetKey(KeyCode.B) == false) {
 			
-			// Update the Spaceships Acceleration
+			if(this.speed < 5.0f)
+				this.speed += 0.05f + (0.05f * 5.0f/(this.speed+1.0f));
 			
-			// Accelerator
-			if((Input.GetKey(KeyCode.W) == true || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) == true) && Input.GetKey(KeyCode.B) == false) {
-				
-				if(this.speed < 2.5f)
-					this.speed += 0.05f;
-				
-				this.rigidbody.velocity += this.transform.forward * verticalAxis * speed;
-			}
-			else {
-				
-				this.speed = 0.0f;
-			}
+			this.rigidbody.velocity += this.transform.forward * verticalAxis * speed;
+		}
+		else {
 			
-			// Brake
-			if(Input.GetKey(KeyCode.B) == true) {
-				
-				this.rigidbody.velocity = this.rigidbody.velocity * 0.9f;
-				this.rigidbody.angularVelocity = this.rigidbody.angularVelocity * 0.9f;
-			}
+			this.speed = 0.0f;
+		}
+		
+		// Brake
+		if(Input.GetKey(KeyCode.B) == true) {
 			
-			if(horizontalAxis != 0) {
-				
-				this.transform.Rotate(this.transform.forward, -horizontalAxis * 0.25f);
-				// Update the Spaceships Steering
-				//this.rigidbody.AddTorque(this.transform.up * horizontalAxis * steeringAngle, ForceMode.VelocityChange);
-				this.rigidbody.AddTorque(this.transform.up * horizontalAxis * steeringAngle, ForceMode.Acceleration);
-			}
+			this.rigidbody.velocity = this.rigidbody.velocity * 0.9f;
+			this.rigidbody.angularVelocity = this.rigidbody.angularVelocity * 0.9f;
+		}
+		
+		if(horizontalAxis != 0) {
+			
+			this.transform.Rotate(this.transform.forward, -horizontalAxis * 0.25f);
+			// Update the Spaceships Steering
+			//this.rigidbody.AddTorque(this.transform.up * horizontalAxis * steeringAngle, ForceMode.VelocityChange);
+			this.rigidbody.AddTorque(this.transform.up * horizontalAxis * steeringAngle, ForceMode.Impulse);
 		}
 	}
 	public void InflictDamage(float damage) {
@@ -227,9 +226,8 @@ public class SpaceshipController : MonoBehaviour {
 		ability.transform.rotation = this.transform.rotation;
 		// Set the Position so that it matches the Spaceships Position
 		ability.transform.position = this.transform.position + this.transform.forward * 1.0f + this.transform.up * 2.0f;
-
-		if(abilityName != "Shield")
-			ability.SetActive(false);
+		// Set the Active state to false so that it doesn't get used
+		ability.SetActive(false);
 		
 		// Add the Ability to the inventory	
 		this.abilityInventory.Add(abilityName, ability);
@@ -245,15 +243,17 @@ public class SpaceshipController : MonoBehaviour {
 		
 		if(backLeftCorner != null && backRightCorner != null && frontLeftCorner != null && frontRightCorner != null) {
 			
+			// Spaceship Rotation Ajustments
 			RaycastHit backLeftHit;
 			RaycastHit backRightHit;
 			RaycastHit frontLeftHit;
 			RaycastHit frontRightHit;
 			
-			bool backLeftRay = Physics.Raycast(backLeftCorner.position, Vector3.down, out backLeftHit, 5.0f, 1 << LayerMask.NameToLayer("Tracks"));
-			bool backRightRay = Physics.Raycast(backRightCorner.position, Vector3.down, out backRightHit, 5.0f, 1 << LayerMask.NameToLayer("Tracks"));
-			bool frontLeftRay = Physics.Raycast(frontLeftCorner.position, Vector3.down, out frontLeftHit, 5.0f, 1 << LayerMask.NameToLayer("Tracks"));
-			bool frontRightRay = Physics.Raycast(frontRightCorner.position, Vector3.down, out frontRightHit, 5.0f, 1 << LayerMask.NameToLayer("Tracks"));
+			// Cast Rays from each of the Spaceships Corners heading towards the Track
+			bool backLeftRay = Physics.Raycast(backLeftCorner.position, -this.transform.up, out backLeftHit, 25.0f, 1 << LayerMask.NameToLayer("Tracks"));
+			bool backRightRay = Physics.Raycast(backRightCorner.position, -this.transform.up, out backRightHit, 25.0f, 1 << LayerMask.NameToLayer("Tracks"));
+			bool frontLeftRay = Physics.Raycast(frontLeftCorner.position, -this.transform.up, out frontLeftHit, 25.0f, 1 << LayerMask.NameToLayer("Tracks"));
+			bool frontRightRay = Physics.Raycast(frontRightCorner.position, -this.transform.up, out frontRightHit, 25.0f, 1 << LayerMask.NameToLayer("Tracks"));
 			
 			if(backLeftRay == true && backRightRay == true && frontLeftRay == true && frontRightRay == true) {
 				
