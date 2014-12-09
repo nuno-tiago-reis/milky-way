f70305e5ac5e551ce75b30bfcb493c41c7c8d1ae
Eduardo Cordeiro <eduardoalcordeiro@gmail.com>
2014-12-09 13:18:46 +0000

    Added first Menus

---
+++ Milky Way/Assets/Scripts/Menus/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    public Menu currentMenu;
+
+	public void Start () {
+
+        showMenu(currentMenu);
+	}
+
+    public void showMenu(Menu menu) {
+
+        if (currentMenu != null)
+            currentMenu.isOpen = true;
+
+        currentMenu.isOpen = false;
+
+        currentMenu = menu;
+        currentMenu.isOpen = true;
+    }
+}
